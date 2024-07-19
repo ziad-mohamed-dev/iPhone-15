a27c4006fa20a14a04c45fc069427c4da0e08dc6
Hunter-Smsm <ziad35199@gmail.com>
2024-07-19 16:04:48 +0300

    test

--- src/app/page.js
+++ src/app/page.js
@@ -1,47 +1,20 @@
-"use client";
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import Highlights from "@/components/Highlights";
 import Model from "@/components/Model";
 import Features from "@/components/Features";
-import { useEffect, useState } from "react";
-import Loader from "@/components/Loader";
+import { Suspense } from "react";
 
 export default function Home() {
-	const [loaded, setLoaded] = useState(false);
-	useEffect(() => {
-		function handelWindowLoad() {
-			setLoaded(true);
-		}
-		window.addEventListener("load", handelWindowLoad);
-		console.log("added");
-		return () => {
-			window.removeEventListener("load", handelWindowLoad);
-			console.log("added");
-		};
-	}, []);
-
 	return (
-		<main
-			className="bg-black"
-			id="app"
-			onLoad={() => {
-				console.log("main loaded");
-			}}
-		>
-			{loaded ? (
-				<>
-					<Navbar />
-					<Hero />
-					<Highlights />
-					<Model />
-					<Features />
-				</>
-			) : (
-				<div className="flex-center h-[100dvh]">
-					<Loader width={100} height={100} />
-				</div>
-			)}
+		<main className="bg-black" id="app">
+			<Suspense fallback={<div>loading...</div>}>
+				<Navbar />
+				<Hero />
+				<Highlights />
+				<Model />
+				<Features />
+			</Suspense>
 		</main>
 	);
 }
